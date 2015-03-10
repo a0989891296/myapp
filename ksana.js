495e13f2e79ce2374420bafe676b7960ddd3eb56
Lin-han-bin <a0989891296@gmail.com>
2015-03-10 15:19:11 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 3,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/a0989891296/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145170,
+  277980,
+  826,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T07:05:25.250Z",
+  "2015-03-10T07:05:25.249Z",
+  "2015-03-10T07:05:25.308Z",
+  "2015-03-10T07:17:30.888Z",
+  "2015-03-10T07:05:25.272Z",
+  "2015-03-10T07:06:02.034Z",
+  "2015-03-10T07:05:25.223Z",
+  "2015-03-10T07:05:25.206Z",
+  "2015-03-10T07:05:49.310Z"
+ ],
+ "date": "2015-03-10T07:17:31.850Z"
+})
